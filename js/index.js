19494e900fd45329461696a343da8917a82c4734
태연 <s2401@e-mirim.hs.kr>
2026-03-04 13:14:45 +0900

    화면 수정

--- js/index.js
+++ js/index.js
@@ -1,15 +1,83 @@
+const highlight = document.querySelector(".highlight");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      highlight.classList.add("active");
+    }
+  });
+}, { threshold: 0.5 });
+
+observer.observe(highlight);
+
 const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   centeredSlides: true,
-  loop: true,
+  loop: false,      // ✅ loop 끄기
   spaceBetween: 200,
-  loopAdditionalSlides: 3,
   watchSlidesProgress: true,
 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+const activities = [
+  {
+    title: "IT SHOW",
+    sub: "2학년이 되면 부원들과 함께  IT SHOW에 참여하게 됩니다!",
+    desc: "지브리 등장인물로 알아보는 성격유형테스트 “그대들 어떤 캐릭터처럼 살 것인가” 를 제작했습니다.",
+    top: "/image/activity/itshow01.jpg",
+    images: [
+      "/image/activity/itshow02.jpg",
+      "/image/activity/itshow03.jpg",
+      "/image/activity/itshow04.jpg",
+    ]
+  },
+  {
+    title: "STUDY",
+    sub: "동아리 내에서 자체적으로 진행하는 전공 스터디",
+    desc: "전공 스터디와 함께 1학년끼리 팀별 프로젝트를 진행합니다. ",
+    top: "/image/activity/study01.jpg",
+    images: [
+      "/image/activity/study02.jpg",
+      "/image/activity/study03.jpg",
+      "/image/activity/study04.jpg",
+    ]
+  },
+  {
+    title: "SOCIALIZING",
+    sub: "동아리 내에서 친목을 도모하는 활동을 합니다",
+    desc: "회식과 교내 모임을 통해 선후배 간 친목을 다집니다",
+    top: "/image/activity/social01.jpg",
+    images: [
+      "/image/activity/social02.jpg",
+      "/image/activity/social03.jpg",
+      "/image/activity/social04.jpg",
+    ]
+  }
+];
+
+let currentIndex = 0;
+
+function updateActivity() {
+  const data = activities[currentIndex];
+
+  document.querySelector(".activity_title").textContent = data.title;
+  document.querySelector(".activity_sub").textContent = data.sub;
+  document.querySelector(".activity_desc").textContent = data.desc;
+  document.querySelector(".activity_top img").src = data.top;
+
+  const imgs = document.querySelectorAll(".activity_bottom img");
+  imgs.forEach((img, i) => {
+    img.src = data.images[i];
+  });
+}
+
+document.querySelector(".activity_next").addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % activities.length;
+  updateActivity();
 });
 
 const awards = [
